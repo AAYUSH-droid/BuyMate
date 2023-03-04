d0e167af9484c16aa13e4bba0f474e6c846271b4
aayush <sharma31aayush@gmail.com>
2023-03-04 21:49:09 +0530

    basic route setup

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/productController");
+const {
+  getAllProducts,
+  // createProduct,
+} = require("../controllers/productController");
 
 const router = express.Router();
 
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,8 @@
 const app = require("./app");
 const dotenv = require("dotenv");
-const connectDatabase = require("./config/database");
+
 //config
 dotenv.config({ path: "backend/config/config.env" });
-
-//Connecting to database
-connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
