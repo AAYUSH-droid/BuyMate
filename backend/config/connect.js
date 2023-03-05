071a65da2396c3b9d6382c7942254ea233652999
aayush <sharma31aayush@gmail.com>
2023-03-05 14:15:09 +0530

    db

---
+++ backend/config/connect.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+  return mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to DB"))
+    .catch((err) => console.log(err));
+};
+
+module.exports = connectDB;
