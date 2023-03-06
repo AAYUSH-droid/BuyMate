79276feae1e5bd87b45715f448c37e253b6def2a
aayush <sharma31aayush@gmail.com>
2023-03-06 12:43:32 +0530

    added user login and register feature also used packages like bcrypt, jwt , validator

---
+++ backend/controllers/userController.js
@@ -0,0 +1,59 @@
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncError = require("../middleware/catchAsyncErrors");
+const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
+
+// Register a User
+exports.registerUser = catchAsyncError(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "this is a sample id",
+      url: "profileULR",
+    },
+  });
+
+  // const token = user.getJWTToken();
+
+  // res.status(201).json({
+  //   success: true,
+  //   token,
+  // });
+  sendToken(user, 201, res);
+});
+
+//Login user
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //checking if user has given password and email both
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter Email and Password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  // const token = user.getJWTToken();
+
+  // res.status(200).json({
+  //   success: true,
+  //   token,
+  // });
+  //all these lines of code replaced by sendToken function which  gives success status , user details, token(stored in cookie);
+  sendToken(user, 200, res);
+});
