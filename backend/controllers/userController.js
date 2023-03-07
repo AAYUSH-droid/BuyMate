e4cff627fde77b21ad0a4511734e1f8b542bc1c1
aayush <sharma31aayush@gmail.com>
2023-03-08 00:13:53 +0530

    forgot password and reset password functinality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncError = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
 
 // Register a User
 exports.registerUser = catchAsyncError(async (req, res, next) => {
@@ -72,3 +73,43 @@ exports.logout = catchAsyncError(async (req, res, next) => {
 });
 
 //Forgot Password
+exports.forgotPasswords = catchAsyncError(async (req, res, next) => {
+  //first find user through email
+  const user = await User.find({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+
+  //user found
+  //Get password token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetPasswordUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/password/reset/${resetToken}`;
+
+  const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: `Ecommerce Password Recovery`,
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} successfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorHander(error.message, 500));
+  }
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,11 +3,16 @@ const {
   registerUser,
   loginUser,
   logout,
+  forgotPasswords,
 } = require("../controllers/userController");
 const router = express.Router();
 
 router.route("/register").post(registerUser);
+
 router.route("/login").post(loginUser);
+
+router.route("./password/forgot").post(forgotPasswords);
+
 router.route("/logout").get(logout);
 
 module.exports = router;
