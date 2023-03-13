8abad8f06592c2971df2a729eec7b813ae909c22
aayush <sharma31aayush@gmail.com>
2023-03-13 23:14:39 +0530

    header layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
 import "./App.css";
+import React from "react";
 import Header from "./component/layout/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
+import webFont from "webfontloader";
 
 function App() {
-  return <Header />;
+  React.useEffect(() => {
+    webFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
 }
 
 export default App;
