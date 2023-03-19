f4624a4a4ae856654ea7411b5daa0b67407e7c4c
aayush <sharma31aayush@gmail.com>
2023-03-19 14:08:46 +0530

    errorHandler function

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -33,3 +33,16 @@ module.exports = (err, req, res, next) => {
     message: err.message,
   });
 };
+
+/*
+In errorHandler file in utils we added Error.captureStackTrace(this, this.constructor) captureStackTrace which captures the whole stack 
+where the error might be this calling it in this file error.js {error: err.stack} it gives the exact location of err, ****
+but calling like {error: err} just gives error message
+
+
+**o/p when {error: err.stack} is called on line 9
+{
+    "success": false,
+    "error": "Error: Product not found\n    at exports.getProductDetails (/Users/aayushsharma/Desktop/BuyMate/backend/controllers/productController.js:33:17)\n    at processTicksAndRejections (internal/process/task_queues.js:93:5)"
+}
+*/
